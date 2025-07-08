8cedff1aeca6a4b01ff70f5d9fb58268b3997d4f
Bartra404 <bartrashin@gmail.com>
2025-07-08 00:46:36 +0000

    submit commit 3

--- src/scripts/engine.js
+++ src/scripts/engine.js
@@ -1,34 +1,35 @@
-
 const state = {
-    view: {
-        squares: document.querrySelectorAll(".square"),
-        enemy: document.querrySelector("enemy"),
-        timeLeft: document.querrySelector("#time-left"),
-        score: document.querrySelector("#score"),
-},
-    value: {
-        timerId: null,
-        gameVelocity: 1000,
-    },
+  view: {
+    squares: document.querrySelectorAll(".square"),
+    enemy: document.querrySelector(".enemy"),
+    timeLeft: document.querrySelector("#time-left"),
+    score: document.querrySelector("#score"),
+  },
+  value: {
+    timerId: null,
+    gameVelocity: 1000,
+  },
+};
+
+function randomSquare() {
+  state.view.squares.forEach((square) => {
+    square.classList.remove("enemy");
+  });
+  let randomNumber = Math.floor(Math.random() * 9);
+  let randomSquare = state.view.squares[randomNumber];
+  randomSquare.classList.add("enemy");
 }
 
-function randomSquare () {
-    state.view.square.forEach((squares) => {
-        squares.classList.remove("enemy");
-    })
-    let randomNumber = Math.floor(Math.random() * 9);
-    let randomSquare = state.view.square[randomNumber];
-    randomSquare.classList.add("enemy");
+function moveEnemy() {
+  state.value.timerId = setInterval(randomSquare, state.value.gameValocity);
 }
 
-function moveEnemy () {
-    state.value.timerId = setInterval (randomSquare, state.value.gameValocity);
+function addListenerHitBox() {
+  state.view.square.forEach((squares) => {});
 }
 
-function addListenerHitBox () {
-    state.view.square.forEach((squares) => {});
+function initialize() {
+  moveEnemy();
 }
 
-function initialize () {
-    moveEnemy ();
-}
+initialize();
